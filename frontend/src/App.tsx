281de332ee624923609214b82f2877da2382c29e
Johannes Helleve <Johanshelleve@gmail.com>
2024-02-24 16:02:22 +0100

    added some components, removed exscasory code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import useFetchData from './FetchData'
+import Task from './Task';
 
 function App() {
-  const [count, setCount] = useState(0);
   const { data, loading, error } = useFetchData();
   const [todos, setTodos] = useState<string>("");
 
   function handleAdd() {
+  }
 
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to asdfsdasdfasdfasdfasdffasdfsftest HMR
-        </p>
         <div>
           <input type="text" value={todos} onChange={(e) => setTodos(e.target.value)}/>
           <button onClick={handleAdd}>Add</button>
@@ -39,15 +22,11 @@ function App() {
             {error && <div>There was an error</div>}
             {loading && <div>Loading...</div>}
             {data.map((item) => (
-              <div key={item.id}>{item.task}</div>
+              <Task key={item.id} task={item.task} />
             ))
             }
           </p>
         </div>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ frontend/src/Task.tsx
@@ -0,0 +1,14 @@
+
+interface TaskProps {
+    task: string;
+}
+
+const Task = ({task}: TaskProps) => {
+    return (
+        <div className="bg-slate-500">
+            {task}
+        </div>
+    )
+}
+
+export default Task;
